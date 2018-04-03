19a5d8e2604fb94d96eb378bdcb547f177139422
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-03 05:57:37 +0200

    init project

---
+++ program.js
@@ -0,0 +1,23 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+  var input = process.stdin.read();
+  if (input !== null) {
+    var instruction = input.toString().trim();
+    switch (instruction) {
+      case '/exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case '/node':
+        node = process.versions.node;
+        process.stdout.write(node + '\n');
+        break;
+      case '/lang':
+        lang = process.env.lang;
+        process.stdout.write(lang + '\n');
+        break;
+      default:
+        process.stderr.write('Wrong instruction!\n');
+    }
+  }
+});
