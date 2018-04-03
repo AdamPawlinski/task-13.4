6ced76bb95c0368a5c190889109629c986801b6f
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-03 06:30:21 +0200

    OSinfo module ready

---
+++ modules/OSinfo.js
@@ -0,0 +1,23 @@
+var os = require('os');
+
+function getOSinfo() {
+  var type = os.type();
+  var release = os.release();
+  var cpu = os.cpus()[0].model;
+  var uptime = os.uptime();
+  var userInfo = os.userInfo();
+
+  if (type === "Darwin") {
+    type = "OSX";
+  } else if (type === "Windows_NT") {
+    type = "Windows";
+  };
+  console.log("System: ", type);
+  console.log("Release: ", release);
+  console.log("CPU model: ", cpu);
+  console.log("Uptime: ~", (uptime / 60).toFixed(), "min");
+  console.log("User name: ", userInfo.username);
+  console.log("User name: ", userInfo.homedir);
+};
+
+exports.print = getOSinfo;
